e77240bf66ab9f3cab38136d43155b5b492341f4
Jeremy Weinstein <jeremydw@gmail.com>
2017-11-22 16:17:24 -0800

    Add CorsMiddleware.

--- backend/main.py
+++ backend/main.py
@@ -40,6 +40,32 @@ class SubmitApplicationHandler(webapp2.RequestHandler):
         self.response.out.write(resp.text)
 
 
+def CorsMiddleware(app):
+
+    def _set_headers(headers):
+        headers.append(('Access-Control-Allow-Origin', '*'))
+        headers.append(('Access-Control-Allow-Methods', '*'))
+        headers.append(('Access-Control-Allow-Headers', 'origin, content-type, accept'))
+        return headers
+
+    def middleware(environ, start_response):
+        if environ.get('REQUEST_METHOD') == 'OPTIONS':
+            headers = []
+            headers = _set_headers(headers)
+            start_response('200 OK', headers)
+            return []
+
+        def headers_start_response(status, headers, *args, **kwargs):
+            all_headers = [key.lower() for key, val in headers]
+            if 'access-control-allow-origin' not in all_headers:
+                headers = _set_headers(headers)
+            return start_response(status, headers, *args, **kwargs)
+        return app(environ, headers_start_response)
+
+    return middleware
+
+
 app = webapp2.WSGIApplication([
     ('/_grow/api/submit-application', SubmitApplicationHandler),
 ])
+app = CorsMiddleware(app)
